ddf2663f4361096f7d1f3cd1a596a3ac64aecafe
KowalczykKamil <kamil.kowalczyk1@microsoft.wsei.edu.pl>
2022-05-18 12:52:59 +0200

    jwt

--- routes/verifyJwt.js
+++ routes/verifyJwt.js
@@ -1,4 +1,46 @@
-const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
 
-module.exports = router 
+const verifyJwt = (req,res, next)=>{
+    const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+      if (error) {
+          res.status(403).json("Your Token is valid!");
+      }
+      req.user = user;   
+      next();
+      
+    });
+   
+  } else {
+  
+   res.status(401).json("Unauthorized user.");
+
+  }
+}
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyJwt(req, res, next, () => {
+      if (req.user.id === req.params.id || req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("You are not authorized!");
+      }
+    });
+  };
+
+  const verifyTokenAndAdmin = (req, res, next) => {
+    verifyJwt(req, res, () => {
+      if (req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("You aren't an admin and you can't to do that!");
+      }
+    });
+  };
+
+  
+
+module.exports={verifyJwt, verifyTokenAndAuthorization, verifyTokenAndAdmin};
